206b7fed0d5e41c9655ce1612dd11244f2bc2527
matheus santiago <matheusmacedo90000@gmail.com>
2025-04-14 10:51:13 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+app.post('/users', (req, res) => {
+    users
+    res.status(201).jason(users);
+})
+
+app.get('/users',(req, res) => {
+    res.status(200).json(users);
+});
+
+app.listen(4000);
